3730220d9b205a4854b531fe72a37333f1dac1fb
rnzndrei <renzandreidelrosario@gmail.com>
2025-02-08 09:58:36 +0800

    first-commit

---
+++ DelRosario-Basics-And-Fundamentals.cpp
@@ -0,0 +1,111 @@
+/*DEL ROSARIO RENZ ANDREI   -     IT2C     -     INTEPROG  -    Exercise: Basics and Fundamentals of C++
+Description: A program that accepts elements for 2 arrays and merge the elements into another array in descending order. The array would only accept a total of 20 elements,
+comprising of 10 from array 1 and 10 from array 2. The accepted elements are only numbers. Letters, words, and symbols would not be accepted*/
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+void line (){
+    cout << "\n-----------------------------------------------------------------" << endl;
+}
+void inputSize (int &size, const string& text){
+    cout << "\nHow many elements would you like to enter in " << text;
+        while (true) {  
+            cin >> size;
+            
+            // Check if the input failed
+            if (cin.fail() || cin.peek() != '\n' || size < 1 || size > 10) {  
+                cout << "\nInvalid input. Please enter a valid number (Minimum of 1 and Maximum of 10): ";
+                cin.clear(); 
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Remove input
+            } else {
+                break;  // Valid input, exit loop
+            }
+        }
+}
+
+void inputElements (int size, float array[], const string& text) {
+    cout << "\nInput " << size << " numbers in the " << text << endl;
+        for (int i = 0; i < size; i++){
+        cout << "\tIndex " << i << ": ";
+            while (true) {
+            cin >> array [i];
+                // Check for invalid input or multiple numbers
+                    if (cin.fail() || cin.peek() != '\n') {
+                        cout << "Invalid input. Please enter a single number in Index " << i << ": ";
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Remove Input
+                    } else {
+                        break;  // Valid input, exit loop
+                    }
+            }
+        }
+}
+
+int main (){
+    char ans;
+
+    do {
+    //Input the size and elements of Array 1
+    int sizeArr1 = 0;
+    line(); cout << "\t\t\tELEMENTS OF ARRAY 1"; line ();
+        inputSize (sizeArr1, "Array 1 (1-10): ");
+            float numArr1 [sizeArr1] = {};
+                inputElements (sizeArr1, numArr1, "Array 1 elements: ");
+
+    //Input the size and elements of Array 2
+    int sizeArr2 = 0;
+    line(); cout << "\t\t\tELEMENTS OF ARRAY 2"; line ();
+        inputSize (sizeArr2, "Array 2 (1-10): ");
+            float numArr2 [sizeArr2] = {};
+                inputElements (sizeArr2, numArr2, "Array 2 elements: ");
+
+    //Merge Array 1 and Array 2 to a single Array
+    int mergedSize = sizeArr1 + sizeArr2;
+    float mergedArr [mergedSize] = {};
+        for (int i = 0; i < sizeArr1; i++) {
+            mergedArr[i] = numArr1[i];
+        }
+        for (int i = 0; i < sizeArr2; i++) {
+            mergedArr[sizeArr1 + i] = numArr2[i];
+        }
+
+    //Bubble Sort
+    float temp;
+        for (int start = 0; start < mergedSize - 1; start++) {
+            for (int i = 0; i < mergedSize - 1 - start; i ++){
+                if (mergedArr[i] < mergedArr[i + 1]) {
+                    temp = mergedArr[i];
+                    mergedArr[i] = mergedArr[i + 1];
+                    mergedArr[i + 1] = temp;
+                }
+            }
+        }
+
+    //Print merged Array in a descending order
+    line();
+    cout << "Array Elements in Descending Order: " << endl;
+    cout << "\t";
+        for (int i = 0; i < mergedSize; i++) {
+            cout << mergedArr[i] << " ";
+        }
+    line();
+
+    while (true) {
+            cout << "\nDo you want to input another set of arrays? (Y/N): ";
+            cin >> ans;
+                if (cin.fail() || cin.peek() != '\n') {
+                    cout << "Invalid input. Please enter Y or N." << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                } else if (ans == 'Y' || ans == 'y' || ans == 'N' || ans == 'n') {
+                    break;  // Valid input
+                } else {
+                    cout << "Invalid input. Please enter only Y or N." << endl;
+                }
+        }
+    } while (ans == 'Y' || ans == 'y');
+
+    return 0;
+}
